be9929eccfb3f4084d2dcd64b1b45a323aa2a8ed
Tatyana <tawita@yandex.ru>
2022-08-09 21:22:38 +0500

    add exam010 найти число в массиве

---
+++ exam010/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // если в массиве несколько одинаковых чисел, то эта команда прерывает поиск после первого найденного числа
+    }
+    //index = index + 1;
+    index++;
+}
+
