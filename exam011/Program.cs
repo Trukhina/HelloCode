c0c2955b80a8a87efc334aed9f5a139508afd279
Trukhina <110234846+Trukhina@users.noreply.github.com>
2022-08-14 20:01:08 +0500

    Добавили пояснение

--- exam011/Program.cs
+++ exam011/Program.cs
@@ -26,7 +26,7 @@ int IndexOf(int[] collection, int find)
 {
     int count = collection.Length;
     int index = 0;
-    int position = -1;
+    int position = -1; // Если ввели число, которого не может быть в массиве, то 0 заменяем на -1 и тогда покажет, что такого числа не может быть. Если оставим 0, то индекс покажет 0-позицию, а это ошибка!
 
     while (index < count)
     {
